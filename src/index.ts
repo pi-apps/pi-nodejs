1a5ecc6bd902b34e6efbb24c4712c60ff0a4e982
Pasniczy <bartoszpasnik@gmail.com>
2025-04-17 14:22:05 +0200

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -148,10 +148,6 @@ export default class PiNetwork {
       amount: transactionData.amount.toString(),
     });
 
-    if (!this.NETWORK_PASSPHRASE) {
-      throw new Error("Network passphrase is not set");
-    }
-
     const transaction = new StellarSdk.TransactionBuilder(myAccount, {
       fee: baseFee.toString(),
       networkPassphrase: network,
