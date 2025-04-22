690f504aa9b76e93f49ed81eeb5bf1f65609301d
Bartek Paśnik <bartoszpasnik@gmail.com>
2025-04-22 23:27:16 +0200

    Update src/index.ts
    Co-authored-by: Hakkyung Lee <17398672+hklee93@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -142,7 +142,7 @@ export default class PiNetwork {
 
   public getIncompleteServerPayments = async (): Promise<Array<PaymentDTO>> => {
     try {
-      const response = await this.api.get<{ incomplete_server_paymenets: Array<PaymentDTO> }>(
+      const response = await this.api.get<{ incomplete_server_payments: Array<PaymentDTO> }>(
         "/payments/incomplete_server_payments"
       );
       return response.data.incomplete_server_paymenets;
